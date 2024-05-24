e287787b43cf53de8cd98d2ed27ed40c8a58c9e4
Sunny <1501041097@qq.com>
2024-05-24 21:24:21 +0800

    update

--- 环境.bat
+++ 环境.bat
@@ -1,5 +1,5 @@
 set http_proxy=http://192.168.0.109:7890 & set https_proxy=http://192.168.0.109:7890
-pip install upgrade pip
+pip install --upgrade pip
 pip install requests
 pip install opencv-python
 pip install paddlepaddle
