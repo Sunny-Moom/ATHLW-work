85467d5b9d3180822bfce03b0c0a50e2f99e5d9c
Sunny <1501041097@qq.com>
2024-05-25 10:15:38 +0800

    update

--- nettest.py
+++ nettest.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     ary = star.adb_check()
     os.system(f"start cmd /c python ./netserver.py")
     for devices in ary:
-        os.system(f"start cmd /k python ./hfcq.py {devices}")
+        os.system(f"start cmd /k python ./wsz.py {devices}")
